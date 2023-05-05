4ff758845424495324234fe5a25d06525abdee1f
Lixian Zhang <lixian.97.m@gmail.com>
2023-05-05 15:50:33 +0200

    debug 282A

--- 282A.c
+++ 282A.c
@@ -3,11 +3,13 @@
 int main() {
     int n, x;
     x = 0;
-    char buff[3];
+    char buff[4];
+    buff[3] = 0;
 
     scanf("%d", &n);
     for (int i = 0; i < n; ++i) {
         scanf("%3s", buff);
+        printf("%s\n", buff);
         if (buff[1] == '+') ++x;
         else --x;
     }
