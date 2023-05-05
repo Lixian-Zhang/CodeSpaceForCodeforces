c7f5fb745eef344612f3528f68aad00e64d97244
Lixian Zhang <lixian.97.m@gmail.com>
2023-05-05 15:33:03 +0200

    add 282A

---
+++ 282A.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n, x;
+    x = 0;
+    char buff[3];
+    
+    for (int i = 0; i < n; ++i) {
+        scanf("%3s", buff);
+        if (buff[1] == '+') ++x;
+        else --x;
+    }
+    printf("%d", x);
+
+    return 0;
+}
